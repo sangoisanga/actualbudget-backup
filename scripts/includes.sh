cfa386f72f64f3427b576a636fa051cac66a3420
sangoisanga <sangoisanga.dev@gmail.com>
2026-05-28 08:31:23 +0700

    refactor: set Telegram silence in status case

--- scripts/includes.sh
+++ scripts/includes.sh
@@ -120,15 +120,20 @@ function send_telegram() {
         return 0
     fi
 
+    local disable_notification
+
     case "${STATUS}" in
     start)
         [[ "${TELEGRAM_WHEN_START}" == "TRUE" ]] || return 0
+        disable_notification="true"
         ;;
     success)
         [[ "${TELEGRAM_WHEN_SUCCESS}" == "TRUE" ]] || return 0
+        disable_notification="true"
         ;;
     failure)
         [[ "${TELEGRAM_WHEN_FAILURE}" == "TRUE" ]] || return 0
+        disable_notification="false"
         ;;
     *)
         color red "unsupported telegram notification status, only supports start, success, failure"
@@ -143,13 +148,13 @@ ${CONTENT}"
     PAYLOAD="$(jq -n \
         --arg chat_id "${TELEGRAM_CHAT_ID}" \
         --arg text "${TEXT}" \
-        --arg status "${STATUS}" \
         --arg parse_mode "${TELEGRAM_PARSE_MODE}" \
         --arg message_thread_id "${TELEGRAM_MESSAGE_THREAD_ID}" \
+        --argjson disable_notification "${disable_notification}" \
         '{
             chat_id: $chat_id,
             text: $text,
-            disable_notification: ($status != "failure")
+            disable_notification: $disable_notification
         }
         + (if $parse_mode != "" then { parse_mode: $parse_mode } else {} end)
         + (if $message_thread_id != "" then { message_thread_id: ($message_thread_id | tonumber) } else {} end)')"
